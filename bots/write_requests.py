abec3a6673e29b21796ccbedec860a26a9b7c45d
Zenn1t <114291264+Mark-Reshetov@users.noreply.github.com>
2022-12-11 02:00:09 +0200

    Написал доки

--- bots/write_requests.py
+++ bots/write_requests.py
@@ -1,35 +1,48 @@
 import sqlite3
 from typing import Any
 
-
-def get_requests_from_one_bot(name_bot: str = None, token_bot: str = None) -> list[Any]:
-    if name_bot is None and token_bot is None:
-        raise "bot or token name must not be empty"
-
-    con = sqlite3.connect("db.sqlite3")
+# Читаем бд по имени или токену
+def get_requests_from_one_bot(path_db: str, name_bot: str = None, token_bot: str = None) -> list[Any]:
+    con = sqlite3.connect(path_db)
     cursor = con.cursor()
-
+    # get_all_info содержит запросы
     if name_bot is not None:
         get_all_info = f"""SELECT token, name_bot, target_type, target_message, answer FROM main WHERE name_bot = '{name_bot}'"""
     else:
         get_all_info = f"""SELECT token, name_bot, target_type, target_message, answer FROM main WHERE token = '{token_bot}'"""
 
     cursor.execute(get_all_info)
-    info = cursor.fetchall()
+    requests = cursor.fetchall()
 
-    return info
+    # возвращаем запросы
+    return requests
 
-
+# Читаем с код и возвращает список содержащий в себе строки с файла
 def read_exemple_file() -> list:
     with open('exemple_bot_script.py', 'r', encoding='utf8') as f:
         return f.readlines()
 
-def write_file_answer(name_bot='test') -> str:
-    with open('aa.py', 'w', encoding='utf8') as f:
+
+# outfile - файл в который запишем код, который отправим позже на сайт
+# path_db - путь к базеданных
+# name_bot и token_bot - имя ИЛИ токен бота в бд по которому ищем запросы
+def write_file_answer(outfile: str, path_db: str, name_bot='', token_bot=''):
+    #открывем файл куда запишем код бота с запросами
+    with open(outfile, 'w', encoding='utf8') as f:
+        # текст с кодом бота
         text_from_exemple = read_exemple_file()
-        requests_for_bot = get_requests_from_one_bot(name_bot=name_bot)
 
+        # проверяем имя и бота, если пустое кидаем ошибку
+        if name_bot is None and token_bot is None:
+            raise "bot or token name must not be empty"
+        elif name_bot is not None:
+            # получаем запросы бота по имени
+            requests_for_bot = get_requests_from_one_bot(path_db, name_bot=name_bot)
+        else:
+            # получаем запросы бота по токену
+            requests_for_bot = get_requests_from_one_bot(path_db, token_bot=token_bot)
+
+        # редактируем 4 строку в файле
         text_from_exemple[3] = 'requests = ' + str(requests_for_bot)
-
-        return ''.join(text_from_exemple)
-
+        # запись в файл
+        f.write(''.join(text_from_exemple))
