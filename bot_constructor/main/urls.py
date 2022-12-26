a7903c0cd145ce95213582ae30d4e0b81ab15857
ihatemodernity <markmurpheen@gmail.com>
2022-12-26 17:12:06 +0200

    Login/Logout button

--- bot_constructor/main/urls.py
+++ bot_constructor/main/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
-from .views import index
+from .views import index, about
 
 urlpatterns = [
     path('', index, name='home'),
-    path('about/', index,name='about'),
+    path('about/', about,name='about'),
 ]
--- bot_constructor/main/views.py
+++ bot_constructor/main/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 def about(request):
-    return render(request, '')
+    return render(request, 'main/about.html')
 
 def index(request):
     """Функция для главной страницы"""
     return render(request, 'main/index.html')
+
