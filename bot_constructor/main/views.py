5821eb81a07f205e164debf6d0df7ea3134f0846
ihatemodernity <markmurpheen@gmail.com>
2022-12-28 21:33:22 +0200

    Home page template + small changes

--- bot_constructor/main/views.py
+++ bot_constructor/main/views.py
@@ -2,6 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+import os
+import sys
+sys.path.append(os.path.join('../../'))
+
 def about(request):
     return render(request, 'main/about.html')
 
